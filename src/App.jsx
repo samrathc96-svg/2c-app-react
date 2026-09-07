203f6ae910bbf8595ca42bcdaf72de2678f85c2a
Samrath <samrath.c96@gmail.com>
2026-09-07 16:47:02 +0200

    Affichage de la liste des metiers avec icones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import './App.css'
 
+const metiers = [
+  { nom: 'Maçonnerie & Gros œuvre', icone: 'bricks' },
+  { nom: 'Plâtrerie & Cloisons', icone: 'layers' },
+  { nom: 'Peinture & Finitions', icone: 'brush' },
+  { nom: 'Plomberie & Sanitaire', icone: 'droplet' },
+  { nom: 'Électricité', icone: 'lightning-charge' },
+  { nom: 'Menuiserie & Serrurerie', icone: 'wrench' },
+  { nom: 'Carrelage & Revêtements', icone: 'grid-3x3' },
+  { nom: 'Couverture & Étanchéité', icone: 'house' },
+  { nom: 'Chauffage & Climatisation', icone: 'fan' }
+]
+
 function App() {
   return (
     <div className="app">
@@ -8,6 +20,16 @@ function App() {
         <span className="chiffre">2</span>
       </div>
       <p className="slogan">Du rayon au chantier, en un clic.</p>
+
+      <h3>Corps de métier</h3>
+      <ul className="liste-metiers">
+        {metiers.map((metier) => (
+          <li key={metier.nom}>
+            <span className="icon"><i className={`bi bi-${metier.icone}`}></i></span>
+            <span>{metier.nom}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
