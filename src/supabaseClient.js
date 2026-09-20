649d54d048f61fb0b456e8393a2fd38eec8ee7b7
Samrath <samrath.c96@gmail.com>
2026-09-20 15:16:32 +0200

    Utilisation de la clef anon legacy pour corriger l'authentification

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-
+ 
 const supabaseUrl = 'https://lmbehxtxgkcrwtwcctgn.supabase.co'
-const supabaseKey = 'sb_publishable_dottvRca4ZIna9Uzmf3oHg_SbP_xCnK'
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtYmVoeHR4Z2tjcnd0d2NjdGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3OTQyNjcsImV4cCI6MjEwNDM3MDI2N30.waSpn8pTIJZKJEAH7X4EJ83WuZENdKkrRRVV-XqZfXw'
+ 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+ 
